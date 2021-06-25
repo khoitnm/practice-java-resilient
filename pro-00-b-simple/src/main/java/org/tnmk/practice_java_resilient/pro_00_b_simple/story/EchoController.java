047fe807bbd48a529e0b75cfe244e58e559a1182
trank <Kevin.Tran@pointclickcare.com>
2021-06-25 17:11:31 -0400

    CircuitBreaker for Echo service.

--- pro-00-b-simple/src/main/java/org/tnmk/practice_java_resilient/pro_00_b_simple/story/EchoController.java
+++ pro-00-b-simple/src/main/java/org/tnmk/practice_java_resilient/pro_00_b_simple/story/EchoController.java
@@ -1,5 +1,6 @@
 package org.tnmk.practice_java_resilient.pro_00_b_simple.story;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,10 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 public class EchoController {
+
+  @CircuitBreaker(name = ResilientSupport.SERVICE_ECHO)
   @GetMapping("/echo")
   public String echo(@RequestParam String message) {
     String echo = "Echo from pro-00-b-simple: " + message;
     log.info(echo);
     return echo;
+  }
+
+  @CircuitBreaker(name = ResilientSupport.SERVICE_ECHO)
+  @GetMapping("/echo/internal-error")
+  public String echoInternalError() {
+    throw new UnsupportedOperationException("Internal error");
   }
 }
---
+++ pro-00-b-simple/src/main/java/org/tnmk/practice_java_resilient/pro_00_b_simple/story/ResilientSupport.java
@@ -0,0 +1,5 @@
+package org.tnmk.practice_java_resilient.pro_00_b_simple.story;
+
+public interface ResilientSupport {
+  String SERVICE_ECHO = "echo";
+}
