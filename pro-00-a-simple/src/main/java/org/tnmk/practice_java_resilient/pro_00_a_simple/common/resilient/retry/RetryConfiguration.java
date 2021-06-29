c25a7b66807142819245850611a2730495b8653a
trank <Kevin.Tran@pointclickcare.com>
2021-06-29 04:40:53 -0400

    Retry with disconnected error

--- pro-00-a-simple/src/main/java/org/tnmk/practice_java_resilient/pro_00_a_simple/common/resilient/retry/RetryConfiguration.java
+++ pro-00-a-simple/src/main/java/org/tnmk/practice_java_resilient/pro_00_a_simple/common/resilient/retry/RetryConfiguration.java
@@ -6,8 +6,6 @@ import io.github.resilience4j.retry.RetryRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.time.Duration;
-
 import static org.tnmk.practice_java_resilient.pro_00_a_simple.common.resilient.ResilientSupport.SERVICE_HELLO;
 
 @Configuration
@@ -15,18 +13,27 @@ public class RetryConfiguration {
 
   @Bean
   public RetryRegistry retryRegistry(RetryOnExceptionPredicate retryOnExceptionPredicate) {
-    //    RetryConfig existingHelloConfig = RetryRegistry.getConfiguration(SERVICE_HELLO).get();
+    RetryConfig helloRetryConfig = helloRetryConfig(retryOnExceptionPredicate);
+
+    // Note: this configuration code looks cleaner, but it doesn't work, I don't know why???
+//    RetryRegistry retryRegistry = RetryRegistry.custom()
+//        .addRetryConfig(SERVICE_HELLO, helloRetryConfig)
+//        .build();
+
+    RetryRegistry retryRegistry = RetryRegistry.ofDefaults();
+    retryRegistry.retry(SERVICE_HELLO, helloRetryConfig);
+    return retryRegistry;
+  }
+
+  private RetryConfig helloRetryConfig(RetryOnExceptionPredicate retryOnExceptionPredicate){
     IntervalFunction intervalWithExponentialBackoff = IntervalFunction
         .ofExponentialBackoff(1000, 2.5d);
 
     RetryConfig retryConfig = RetryConfig.custom()
-        .maxAttempts(3)
+        .maxAttempts(2)
         .intervalFunction(intervalWithExponentialBackoff)
         .retryOnException(retryOnExceptionPredicate)
         .build();
-
-    RetryRegistry retryRegistry = RetryRegistry.of(retryConfig);
-    retryRegistry.retry(SERVICE_HELLO, retryConfig);
-    return retryRegistry;
+    return retryConfig;
   }
 }
