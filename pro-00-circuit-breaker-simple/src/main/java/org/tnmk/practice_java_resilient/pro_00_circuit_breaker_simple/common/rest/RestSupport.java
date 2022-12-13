55128cdfdfa361bf33d2e6b6081f39f52be61413
Khoi Tran <khoi.tnm@gmail.com>
2022-12-12 22:56:27 -0500

    Some minor update.

---
+++ pro-00-circuit-breaker-simple/src/main/java/org/tnmk/practice_java_resilient/pro_00_circuit_breaker_simple/common/rest/RestSupport.java
@@ -0,0 +1,5 @@
+package org.tnmk.practice_java_resilient.pro_00_circuit_breaker_simple.common.rest;
+
+public interface RestSupport {
+  static String API_PREFIX = "/api/v1/pro-00-circuit-breaker-simple";
+}
--- pro-00-circuit-breaker-simple/src/main/java/org/tnmk/practice_java_resilient/pro_00_circuit_breaker_simple/story/HelloController.java
+++ pro-00-circuit-breaker-simple/src/main/java/org/tnmk/practice_java_resilient/pro_00_circuit_breaker_simple/story/HelloController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.tnmk.practice_java_resilient.pro_00_circuit_breaker_simple.common.rest.RestSupport;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,7 +12,7 @@ public class HelloController {
 
   private final HelloService helloService;
 
-  @GetMapping("/hello")
+  @GetMapping(RestSupport.API_PREFIX + "/hello")
   public String hello(@RequestParam(value = "success", required = false, defaultValue = "true") boolean success) {
     helloService.excecute(success);
     return "Finished";
